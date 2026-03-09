a7112541109de7c3fee980d7f1338e22467132bb
melkarobera123-commits <melkarobera123@gmail.com>
2026-03-09 11:18:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-﻿console.log('Apothecaries Blog loaded');
+﻿const searchBar = document.getElementById("searchBar");
+const posts = document.querySelectorAll(".post");
+
+searchBar.addEventListener("keyup", function(){
+
+let value = searchBar.value.toLowerCase();
+
+posts.forEach(post => {
+
+let text = post.innerText.toLowerCase();
+
+post.style.display = text.includes(value) ? "block" : "none";
+
+});
+
+});
+
+
+const toggle = document.getElementById("darkToggle");
+
+toggle.addEventListener("click", function(){
+
+document.body.classList.toggle("dark");
+
+});
